cf000db48db15d18ce8245bd5a7d0209d272a426
kleine-banane <kleine-banane@outlook.de>
2025-03-02 22:20:04 +0100

    Player und movement hinzugefügt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -8,13 +9,22 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill((0, 0, 0))
+        player1.update(dt)
+        player1.draw(screen)
 
         pygame.display.flip()
-        
+        dt = clock.tick(60) / 1000
+
 if __name__ == "__main__":
     main()
