3a260b88356d0e60ccd316c3d2b3ff7a7046682a
kleine-banane <kleine-banane@outlook.de>
2025-03-03 20:03:38 +0100

    Asteroiden spawnen jetzt

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from constants import *
 from player import *
 from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
@@ -13,14 +14,16 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    asteroidfield = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
-
+    AsteroidField.containers = (updatable)    
 
     clock = pygame.time.Clock()
     dt = 0
 
     player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroidfield1 = AsteroidField()
 
     while True:
         for event in pygame.event.get():
