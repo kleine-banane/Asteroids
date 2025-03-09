f96e160e8baa7539cab3718d5db9ca6bc98f152d
kleine-banane <kleine-banane@outlook.de>
2025-03-09 21:21:00 +0100

    Schüsse fliegen nicht mehr unbegrenzt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # to start venv: source venv/bin/activate
 
+import os
 import sys
 import pygame
 from constants import *
@@ -10,10 +11,13 @@ from shot import *
 
 def main():
     pygame.init()
+    pygame.font.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    score = 0
+    font = pygame.font.Font(None, 36)
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
@@ -25,7 +29,7 @@ def main():
     Player.containers = (wrappable, updatable, drawable)
     Asteroid.containers = (wrappable, asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
-    Shot.containers = (shots, updatable, drawable)    
+    Shot.containers = (wrappable, shots, updatable, drawable)    
 
     clock = pygame.time.Clock()
     dt = 0
@@ -41,21 +45,29 @@ def main():
         screen.fill((0, 0, 0))
         for update in updatable:
             update.update(dt)
+            score_text = font.render(str(score), True, (255, 255, 255))
 
         for asteroid in asteroids:
             if asteroid.check_collision(player):
                 print("Game Over!")
-                sys.exit()
+                asteroidfield.kill()
+                for asteroid in asteroids:
+                    asteroid.kill()
+                player.kill()
+                game_over = font.render(f"Your score is: {score}!", True, (255, 255, 255))
+                screen.blit(game_over, (SCREEN_HEIGHT/2, SCREEN_WIDTH/2))
             
             for shot in shots:
                 if asteroid.check_collision(shot):
                     asteroid.split()
                     shot.kill()
+                    score += 1
         
         for wrap in wrappable:
             wrap.wrap_around()
 
-        for draw in drawable:        
+        for draw in drawable:
+            screen.blit(score_text, (10, 10))        
             draw.draw(screen)
 
         pygame.display.flip()
--- shot.py
+++ shot.py
@@ -1,15 +1,24 @@
 import pygame
 from circleshape import *
-from constants import SHOT_RADIUS
+from constants import *
 
 class Shot(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, SHOT_RADIUS)
+        self.travel_distance = 0
+        self.old_position_x = 0
+        self.old_position_y = 0
         
     def draw(self, screen):
         pygame.draw.circle(screen, (255, 255, 255), self.position, self.radius, 2)
 
     def update(self, dt):
-        self.position += (self.velocity * dt)
-
-    
+        self.old_position_x = self.position.x
+        self.old_position_y = self.position.y
+        self.position += (self.velocity * dt)      
+        delta_x = self.position.x - self.old_position_x
+        delta_y = self.position.y - self.old_position_y
+        self.travel_distance += ((delta_x ** 2) + (delta_y ** 2)) ** 0.5
+        
+        if self.travel_distance >= MAX_SHOT_DISTANCE:
+            self.kill()
